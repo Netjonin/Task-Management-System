f41fc9178b042dc0c30f4f6cdb9a99563fd8a276
netjonin <pencilpepeyo@yahoo.com>
2024-02-22 12:40:51 +0100

    readIDParam added

---
+++ cmd/api/routes.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"net/http"
+	"github.com/julienschmidt/httprouter"
+)
+
+func (app *application) routes() *httprouter.Router {
+	router := httprouter.New()
+	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
+	router.HandlerFunc(http.MethodPost, "/v1/tasks", app.createTaskHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/tasks/:id", app.showTaskHandler)
+	return router
+}
