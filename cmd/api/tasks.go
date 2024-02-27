0966d46cd99ca319a7df7df29ebccb25a1fa42d9
netjonin <pencilpepeyo@yahoo.com>
2024-02-27 22:46:24 +0100

    validation added

--- cmd/api/tasks.go
+++ cmd/api/tasks.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"TMS.netjonin.net/internal/data"
+	"TMS.netjonin.net/internal/validator"
 )
 
 func (app *application) createTaskHandler(w http.ResponseWriter, r *http.Request) {
@@ -22,6 +23,31 @@ func (app *application) createTaskHandler(w http.ResponseWriter, r *http.Request
 		app.badRequestResponse(w, r, err)
 		return
 	}
+	v := validator.New()
+
+	v.Check(input.Title != "", "title", "must be provided")
+	v.Check(len(input.Title) <= 500, "title", "must not be more than 500 bytes long")
+
+	v.Check(input.Description != "", "description", "must be provided")
+	v.Check(!input.Expired, "expired", "newly created task should be active")
+	v.Check(input.Status != "", "status", "newly created task should be in To-Do")
+
+	// v.Check(input.Year != 0, "year", "must be provided")
+	// v.Check(input.Year >= 1888, "year", "must be greater than 1888")
+	// v.Check(input.Year <= int32(time.Now().Year()), "year", "must not be in the future")
+	// v.Check(input.Runtime != 0, "runtime", "must be provided")
+	// v.Check(input.Runtime > 0, "runtime", "must be a positive integer")
+	// v.Check(input.Genres != nil, "genres", "must be provided")
+	// v.Check(len(input.Genres) >= 1, "genres", "must contain at least 1 genre")
+	// v.Check(len(input.Genres) <= 5, "genres", "must not contain more than 5 genres")
+	
+	// v.Check(validator.Unique(input.Genres), "genres", "must not contain duplicate values")
+
+	if !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
 	fmt.Fprintf(w, "%+v\n", input)
 
 }
