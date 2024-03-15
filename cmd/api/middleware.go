71ac2d4af823b46863f320f8a4fe51a5106a7e4a
netjonin <pencilpepeyo@yahoo.com>
2024-03-15 07:52:21 +0100

    IP-based Rate limiter added

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"net/http"
+	"sync"
+	"time"
 
 	"golang.org/x/time/rate"
 )
 
 func (app *application) recoverPanic(next http.Handler) http.Handler {
+
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		defer func() {
 			if err := recover(); err != nil {
@@ -20,16 +24,61 @@ func (app *application) recoverPanic(next http.Handler) http.Handler {
 	})
 }
 
+// Using this pattern for rate-limiting will only work if your API application is running on a
+// single-machine. If your infrastructure is distributed, with your application running on
+// multiple servers behind a load balancer, then you’ll need to use an alternative approach.
+// If you’re using HAProxy or Nginx as a load balancer or reverse proxy, both of these have
+// built-in functionality for rate limiting that it would probably be sensible to use.
+// Alternatively, you could use a fast database like Redis to maintain a request count for
+// clients, running on a server which all your application servers can communicate with.
+
 func (app *application) rateLimit(next http.Handler) http.Handler {
-	
-	limiter := rate.NewLimiter(2, 4)
+
+	type client struct {
+		limiter  *rate.Limiter
+		lastSeen time.Time
+	}
+	var (
+		mu      sync.Mutex
+		clients = make(map[string]*client)
+	)
+
+	go func() {
+		for {
+			time.Sleep(time.Minute)
+			mu.Lock()
+			for ip, client := range clients {
+				if time.Since(client.lastSeen) > 3*time.Minute {
+					delete(clients, ip)
+				}
+			}
+			mu.Unlock()
+		}
+	}()
+
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
-		// if the bucket is empty
-		if !limiter.Allow() {
-			app.rateLimitExceededResponse(w, r)
-			return
+		if app.config.limiter.enabled {
+			ip, _, err := net.SplitHostPort(r.RemoteAddr)
+			if err != nil {
+				app.serverErrorResponse(w, r, err)
+				return
+			}
+
+			mu.Lock()
+			if _, found := clients[ip]; !found {
+				clients[ip] = &client{limiter: rate.NewLimiter(2, 4)}
+			}
+			clients[ip].lastSeen = time.Now()
+
+			if !clients[ip].limiter.Allow() {
+				mu.Unlock()
+				app.rateLimitExceededResponse(w, r)
+				return
+			}
+			mu.Unlock()
 		}
 		next.ServeHTTP(w, r)
 	})
+
 }
